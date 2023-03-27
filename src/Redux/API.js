66c464a9c52cf045da5f373367d19373b8df3223
kibni <kibni1409@gmail.com>
2023-03-27 18:42:45 +0300

    Мета для http

--- src/Redux/API.js
+++ src/Redux/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://192.168.0.8:8081',
+  baseURL: 'https://192.168.0.8:8081',
   headers: {
     'Content-Type': 'application/json',
   },
